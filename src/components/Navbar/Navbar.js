78389b9beaf3bb88e1c116feb0eadfb31e5bbce5
ranagull <ranagul1236@gmail.com>
2023-10-15 15:44:55 +0300

    first commit home_page

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { withTheme } from "styled-components";
+
+function Navbar(){
+    let userId = 5;
+
+    return(
+        <div>
+
+        <AppBar position="static">
+            <Toolbar>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Link style={{
+                    textDecoration: "none",
+                    boxShadow: "none",
+                    color: "white"
+                }}
+                 to="/">Home</Link>
+            </Typography>
+            <Button color="inherit">Login</Button>
+            <Link to={{pathname: '/users/' + userId}}>User</Link>
+            </Toolbar>
+        </AppBar>
+        </div>
+    )
+}
+export default Navbar;
